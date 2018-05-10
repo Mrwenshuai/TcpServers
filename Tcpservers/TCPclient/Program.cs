112a8c859d78292d308e9e1f6b8b2bd738e856ec
MrWenShuai <1154355867@qq.com>
2018-05-10 17:19:55 +0800

    initial version
    Signed-off-by: 2018年5月10日

---
+++ Tcpservers/TCPclient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("192.168.2.100"), 13141);
+            clientSocket.Connect(ipEndPoint);
+
+            byte[] data = new byte[1024];
+            int count = clientSocket.Receive(data);
+            string msg = Encoding.UTF8.GetString(data, 0, count);
+
+            Console.WriteLine(msg + "  ---->> msg");
+            while (true)
+            {
+                string s = Console.ReadLine();
+                clientSocket.Send(Encoding.UTF8.GetBytes(s));
+            }
+
+            clientSocket.Close();
+        }
+    }
+}
---
+++ Tcpservers/Tcpservers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Tcpservers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartAsyncServer();
+            Console.ReadKey();
+        }
+
+        static void StartAsyncServer()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("192.168.2.100");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 13141);
+            serverSocket.Bind(ipEndPoint);
+            serverSocket.Listen(0);
+
+
+            serverSocket.BeginAccept(BeginAcceptAsync, serverSocket);
+            //
+
+
+        }
+
+        static void BeginAcceptAsync(IAsyncResult ar)
+        {
+            Socket serverSocket = ar.AsyncState as Socket;
+            Socket clientSocket = serverSocket.EndAccept(ar);
+
+            string msg = " hello client , 你好！ ";
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ServerReceiveMsg, clientSocket);
+            serverSocket.BeginAccept(BeginAcceptAsync, serverSocket);
+        }
+
+        static byte[] dataBuffer = new byte[1024];
+        static void ServerReceiveMsg(IAsyncResult ar)
+        {
+            Socket clientSocket = ar.AsyncState as Socket;
+            int count = clientSocket.EndReceive(ar);
+            string msg = Encoding.UTF8.GetString(dataBuffer, 0, count);
+
+            Console.WriteLine("Receive msg form client : " + msg);
+            clientSocket.BeginReceive(dataBuffer, 0, 1024, SocketFlags.None, ServerReceiveMsg, clientSocket);
+        }
+    }
+}
